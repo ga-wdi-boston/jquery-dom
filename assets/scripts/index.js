0483b5296f804e4c4802754ccfb6c5459b541405
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-26 19:23:42 -0500

    Finish integration of js-template into talk
    -   Remove files that were no longer needed
    -   Rename script files to reflect the name of the app
    -   Rename global stylesheet since it is really a style manifest
    -   Re-integrate previous index.html app structure

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,3 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-require('./example');
-
-// load sass manifest
-require('../styles/index.scss');
-
-$(document).ready(() => {
-  console.log('It works.');
-});
+require('./listr');
--- spec/listr.spec.js
+++ spec/listr.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var example = require('../assets/scripts/example');
+var example = require('../assets/scripts/listr');
 
 describe('Example', function() {
   it('is true', function() {
